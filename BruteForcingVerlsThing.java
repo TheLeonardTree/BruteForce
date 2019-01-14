fa00fc78f2446f4bcd2c9e5d4b8e23cc719f0c28
Leonard Treman <leonardtreman@gmail.com>
2019-01-14 14:40:34 -0500

    Add files via upload

---
+++ BruteForcingVerlsThing.java
@@ -0,0 +1,708 @@
+package brute.forcing.verls.thing;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.util.Random;
+import javax.swing.KeyStroke;
+
+public class BruteForcingVerlsThing {
+public static void main(String[] args) throws AWTException, InterruptedException, IOException {
+
+int counter = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+int counter10 = 0;
+int counter11 = 0;
+int counter12 = 0;
+
+Robot robot = new Robot();
+int[] counterinarray = new int[1];
+int max = 9;
+
+while(counter12 <= max)
+{
+while(counter11 <= max)
+{
+while(counter10 <= max)
+{
+while(counter9 <= max)
+{
+while(counter8 <= max)
+{
+while(counter7 <= max)
+{
+while(counter6 <= max)
+{
+while(counter5 <= max)
+{
+while(counter4 <= max)
+{
+while(counter3 <= max)
+{
+while( counter2 <= max )
+{
+while (counter <= max )
+{
+    Runtime runtime = Runtime.getRuntime(); 
+            Process p = runtime.exec("C:\\Users\\verl & cathy\\Desktop\\editNC\\editNC8.exe");     
+            robot.mouseMove (775,600);
+            Runtime.getRuntime().exec("taskkill /f /im C:\\Users\\verl & cathy\\Desktop\\editNC\\editNC8.exe");
+    robot.mousePress(InputEvent.BUTTON1_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_MASK);
+            robot.mouseMove (1025,600);
+    robot.mousePress(InputEvent.BUTTON1_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_MASK);  
+if (counter12 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter12 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter12 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter12 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter12 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter12 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter12 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter12 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter12 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter12 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }   
+if (counter11 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter11 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter11 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter11 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter11 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter11 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter11 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter11 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter11 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter11 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+if (counter10 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter10 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter10 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter10 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter10 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter10 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter10 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter10 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter10 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter10 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+if (counter9 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter9 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter9 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter9 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter9 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter9 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter9 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter9 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter9 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter9 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+if (counter8 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter8 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter8 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter8 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter8 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter8 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter8 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter8 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter8 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter8 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }
+if (counter7 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter7 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter7 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter7 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter7 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter7 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter7 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter7 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter7 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter7 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+if (counter6 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter6 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter6 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter6 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter6 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter6 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter6 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter6 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter6 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter6 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+if (counter5 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter5 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter5 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter5 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter5 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter5 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter5 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter5 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter5 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter5 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+if (counter4 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter4 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter4 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter4 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter4 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter4 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter4 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter4 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter4 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter4 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+if (counter3 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter3 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter3 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter3 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter3 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter3 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter3 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter3 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter3 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter3 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+if (counter2 == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter2 == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter2 == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter2 == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter2 == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter2 == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter2 == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter2 == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter2 == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter2 == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+if (counter == 0)
+        {
+       robot.keyPress(KeyEvent.VK_0);
+       robot.keyRelease(KeyEvent.VK_0);             
+        }
+if (counter == 1)
+        {
+       robot.keyPress(KeyEvent.VK_1);
+       robot.keyRelease(KeyEvent.VK_1);             
+        }
+if (counter == 2)
+        {
+       robot.keyPress(KeyEvent.VK_2);
+       robot.keyRelease(KeyEvent.VK_2);             
+        }
+if (counter == 3)
+        {
+       robot.keyPress(KeyEvent.VK_3);
+       robot.keyRelease(KeyEvent.VK_3);             
+        }
+if (counter == 4)
+        {
+       robot.keyPress(KeyEvent.VK_4);
+       robot.keyRelease(KeyEvent.VK_4);             
+        }
+if (counter == 5)
+        {
+       robot.keyPress(KeyEvent.VK_5);
+       robot.keyRelease(KeyEvent.VK_5);             
+        }
+if (counter == 6)
+        {
+       robot.keyPress(KeyEvent.VK_6);
+       robot.keyRelease(KeyEvent.VK_6);             
+        }
+if (counter == 7)
+        {
+       robot.keyPress(KeyEvent.VK_7);
+       robot.keyRelease(KeyEvent.VK_7);             
+        }   
+if (counter == 8)
+        {
+       robot.keyPress(KeyEvent.VK_8);
+       robot.keyRelease(KeyEvent.VK_8);             
+        }    
+if (counter == 9)
+        {
+       robot.keyPress(KeyEvent.VK_9);
+       robot.keyRelease(KeyEvent.VK_9);             
+        }  
+System.out.println(counter12 + "" + counter11 + "" + counter10 + "" + counter9 + "" + counter8 + "" + counter7 + "" + counter6 + "" + counter5 + "" + counter4 + "" + counter3 + "" + counter2 + "" + counter);
+robot.mouseMove (1125,600);
+    robot.mousePress(InputEvent.BUTTON1_MASK);
+    robot.mouseRelease(InputEvent.BUTTON1_MASK);
+ p.destroy();
+counter = counter + 1; 
+}
+counter2 = counter2 + 1;
+counter = 0;
+}
+counter3 = counter3 + 1;
+counter2 = 0;
+}
+counter4 = counter4 + 1;
+counter3 = 0;
+}
+counter5 = counter5 + 1;
+counter4 = 0;
+}
+Thread.sleep(300000);
+counter6 = counter6 + 1;
+counter5 = 0;
+}
+counter7 = counter7 + 1;
+counter6 = 0;
+}
+counter8 = counter8 + 1;
+counter7 = 0;
+}
+counter9 = counter9 + 1;
+counter8 = 0;
+}
+counter10 = counter10 + 1;
+counter9 = 0;
+}
+counter11 = counter11 + 1;
+counter10 = 0;
+}
+counter12 = counter12 + 1;
+counter11 = 0;
+}
+    }   
+}
